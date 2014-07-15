1ea236deb7260a914c09db3d4e17cbffbab1c7e5
Md Insan <insan.md27@gmail.com>
2014-07-16 01:52:40 +0530

    Revert "Revert "Revert "Revert "Project-insan""""
    This reverts commit 08b04134220aa6a0b33bfb04dc8cf9632df8e20f.

---
+++ s3.php
@@ -0,0 +1,105 @@
+<?php
+$s = $_GET['s'];
+?>
+<HTML>
+<HEAD>
+<script>
+function back()
+{
+x=self.location.replace("s8.php");
+}
+</script>
+
+
+
+
+<TITLE>Marks Details</TITLE>
+
+
+<body>
+<body bgcolor="#FFFFCC">
+<table class="main_body" width="850" align="center" border="0" cellpadding="0" cellspacing="0">
+
+   <tbody><tr>
+
+    <td align="left" valign="top"><table class="" width="80%" align="center" border="0" cellpadding="0" cellspacing="0">
+
+      <tbody><tr>
+        <td width="210" align="left" valign="top"><img src="index_files/LOGO.jpg" alt="galaxy" width="230" height="128">
+        <td width=990" align="right" bgcolor="lightyellow" valign="top">
+       <table style="margin-top: 0px;" width="100%" border="0" cellpadding="0" cellspacing="0">
+<tbody><tr>
+     
+       <td class="style20  style12" style="padding-center: 50px;">
+
+ <center><img src="index_files/LOGO1.jpg" alt="logo" width="556" height="128"></center>
+         </td>
+</tr> </tbody></table>           
+          
+<td width="100" align="left" valign="top"><img src="index_files/ugc.png" alt="ugc" width="214" height="128">
+
+</td>
+
+    </tbody>
+
+      </tr>
+
+     </tbody></table></td>
+
+
+  <tr>
+    <td align="left" valign="top"><table class="main_body" width="1000" border="0" cellpadding="0" cellspacing="0">
+
+      <tbody><tr>
+
+
+        <td width="1000" align="left" height="0" valign="top">
+       <table width="100%" border="0" cellpadding="0" cellspacing="0">
+          <tbody><tr>
+
+            <td><table width="100%" border="0" cellpadding="0" cellspacing="0">
+
+             <center> <tbody>
+              <tr>
+               <td colspan="30" class="line" align="left" valign="top"></td>
+
+              </tr></center>
+
+
+
+<CENTER>
+
+<body>
+<div align="left"><img src="index_files/bord.jpg" height="3" width="100%"></div>
+<td width="900" align="left" bgcolor="FFC35A" valign="top"><table style="margin-top: 0px;" width="100%" border="0" cellpadding="0" cellspacing="0">
+</tbody>
+
+<tr> 
+
+<div class="linkw" align="left" bgcolor="FFC35A"><A HREF = Galaxy.php><img src="index_files/home.jpg" height="50" width="70">&nbsp&nbsp
+<A HREF = s8.php><img src="index_files/lgt.jpg" height="50" width="100"></A>
+<div align="left"><img src="index_files/bord.jpg" height="3" width="100%"></div>
+<br><br><br>
+<center>
+<img src="index_files/plz.jpg" align="top" height="90" type="image" width="300">
+
+
+<H1 style="text-shadow:
+ -3px -2px 0px green"></CENTER></U>
+<br>
+<div align="center"><a href="s6.php"><img src="index_files/adm.jpg" height="100" width="200"></a>
+<a href="s4.php"><img src="index_files/mrk.jpg" height="93" width="180"></a></div>
+
+</table><br><br><br><br><br><tr> 
+</U>
+
+</table>
+
+</h1>
+
+<CENTER>
+
+</head>
+</CENTER>
+</BODY>
+</HTML>
